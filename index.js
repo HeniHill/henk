746c2afec614033a8e3435ebf054413fcd6baba3
henoke <henokemafm@gmail.com>
2021-08-25 13:50:46 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let express=require('express');
+let http=require('http');
+
+let app=express();
+
+let server=http.createServer(app);
+
+app.use("/",(req,res)=>{
+  res.send("WELCOME TO NODE JS");
+});
+
+let port=process.env.PORT || 80;
+
+server.listen(port,()=>console.log(`server started at port ${port}`));
